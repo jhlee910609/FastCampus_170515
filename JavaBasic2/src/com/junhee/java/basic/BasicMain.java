194476e807b43923b7fdfc2178d87754d7d3eb77
JunHee Lee <jhlee910609@gmail.com>
2017-05-15 12:55:31 +0900

    — 오전에 배운 내용 — * 조건문 : if 문, switch 문 * for문, while 문, do - while 문 * drawSnail : 반복문으로 접근하기 // 규칙적인 패턴 찾기

---
+++ JavaBasic2/src/com/junhee/java/basic/BasicMain.java
@@ -0,0 +1,62 @@
+package com.junhee.java.basic;
+
+// 사용하고자 하는 클래스가 다른 패키지에 있을 때는 import로 불러옴 
+import com.junhee.java.basicsub.BasicSub;
+
+public class BasicMain {
+									// 왜 아규먼트? 
+									// 왜 파라미터가 아님?
+	// *** 코딩할 때, 중요하진 않지만 면접 문제 ***
+	// 넘겨주는 값 = 아규먼트, 함수 정의할 때의 값 없는 변수 = 파라미
+	// 값 불러오는 시점이 언제냐에 따라 다르다.
+	// 메서드 작성 시 = 파라미터 (parameter)
+	// 실제로 값을 넣을 때 = 아규먼트 (argument)
+	// args 대신 params 입력해도 됨
+	// 그래서 파라미터와 아규먼트를 '인자'라고 함.
+
+	public static void main(String args[]) {
+		
+		String firstOption = args[0];
+		
+		if(firstOption.equals("-m:)) {
+		
+		
+		
+		
+		
+		
+		// switch 입력 값이 있을 때
+		switch (비교대상) {
+		case 값1 :
+			// TODO
+			
+		case 값2 :
+			// TODO
+			
+		default :
+			// TODO
+			
+		
+		}
+		
+		// if 는 내가 쓰고 싶을 때
+		
+		String a = "스트라이크";
+		String b = "볼";
+		// 반복문
+		for ( ; ;) {
+			
+			// 조건식 사용하지 않아도 무한루프로 실행 
+
+		}
+		
+		while (조건식) {
+			//
+		}
+		
+		do {
+			// 조건과 항관 없이 무조건 한 번 실행하고 싶을 
+		} while (조건식);
+		
+	}
+}
